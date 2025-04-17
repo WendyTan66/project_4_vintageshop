df160e78286b5a4e268dea95e6bd54be6c41feae
Wendy Tan <wendytanvalencia@gmail.com>
2025-04-17 02:49:58 +0100

    product management debug

--- products/forms.py
+++ products/forms.py
@@ -11,8 +11,9 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        category_choices = [(c.id, c.name) for c in categories]
 
-        self.fields['category'].choices = friendly_names
+        self.fields['category'].choices = category_choices
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
