55db8128fb63e285ee3d2d44130bde9eca9a13e7
Wendy Tan <wendytanvalencia@gmail.com>
2025-04-20 23:48:07 +0100

    debug

--- vintageshop/urls.py
+++ vintageshop/urls.py
@@ -26,5 +26,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profiles/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('profiles/', include('profiles.urls')),]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
