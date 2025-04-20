54bc9eb04f8f9604a8365ada4b2e546befdffffc
Wendy Tan <wendytanvalencia@gmail.com>
2025-04-20 22:49:16 +0100

    debug static files

--- vintageshop/settings.py
+++ vintageshop/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*cn0ypz($#a+)bui96qe$(+gn0-7-gp$x0q38l9u4$w8ukn_-j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
